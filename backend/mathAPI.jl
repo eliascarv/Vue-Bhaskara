aa6f20e0431c1aebae8a824326ca86c2f8f306f4
eliascarv <73039601+eliascarv@users.noreply.github.com>
2021-10-05 16:04:14 -0300

    mathAPI v2

--- backend/mathAPI.jl
+++ backend/mathAPI.jl
@@ -14,17 +14,28 @@ function bhaskara(a, b, c)
     return x1, x2
 end
 
-function calculate(req::Request, coef)
+function handle_bhaskara(req::Request)
+    coef = JSON3.read(req.body)
+
     a = coef.a
     b = coef.b
     c = coef.c
     x1, x2 = bhaskara(a, b, c)
-    return Dict("x1" => "$x1", "x2" => "$x2")
+
+    res_body = Dict("x1" => "$x1", "x2" => "$x2")
+    res_headers = [
+        "Access-Control-Allow-Origin" => "*",
+        "Access-Control-Allow-Headers" => "*",
+        "Access-Control-Allow-Methods" => "*",
+        "Content-Type" => "application/json"
+    ]
+
+    return Response(200, res_headers, body = JSON3.write(res_body))
 end
 
 const ROUTER = Router()
 
-@register(ROUTER, "POST", "/bhaskara", calculate)
+@register(ROUTER, "POST", "/bhaskara", handle_bhaskara)
 
 # Get local machine ip
 const IP = getipaddr()
@@ -38,14 +49,4 @@ println("""\n
   /bhaskara       POST
 """)
 
-serve(IP, 8081) do req::Request
-    req_body = payload(req)
-    res_body = handle(ROUTER, req, JSON3.read(req_body))
-    res_headers = [
-        "Access-Control-Allow-Origin" => "*",
-        "Access-Control-Allow-Headers" => "*",
-        "Access-Control-Allow-Methods" => "*",
-        "Content-Type" => "application/json"
-    ]
-    return Response(200, res_headers, body = JSON3.write(res_body))
-end
+serve(ROUTER, IP, 8081)
