0c2a8609d93c22826f0c0b7f9d7eaba4d2b75906
eliascarv <73039601+eliascarv@users.noreply.github.com>
2021-10-07 09:53:27 -0300

    Added a precompilation

--- backend/mathAPI.jl
+++ backend/mathAPI.jl
@@ -33,6 +33,18 @@ function handle_bhaskara(req::Request)
     return Response(200, res_headers, body = JSON3.write(res_body))
 end
 
+# Precompile
+while true
+    p1 = Dict("a" => 1, "b" => 3, "c" => -4)
+    p2 = Dict("a" => 1, "b" => 2, "c" => 4)
+    req1 = Request("POST", "", [], JSON3.write(p1))
+    req2 = Request("POST", "", [], JSON3.write(p2))
+    handle_bhaskara(req1)
+    handle_bhaskara(req2)
+    break
+end
+
+# Creating routes and starting the API
 const ROUTER = Router()
 
 @register(ROUTER, "POST", "/bhaskara", handle_bhaskara)
